07258bb4c3cdf1676cdd87df6bab27f2e7a27b0f
Marcos Vinicius Silva <MrcsVS@outlook.com>
2022-09-01 12:53:13 -0300

    finished project

--- interactive-rating-component-main/main.js
+++ interactive-rating-component-main/main.js
@@ -3,16 +3,24 @@
 const buttons = document.querySelector('.buttons')
 const scores = document.querySelectorAll('.score')
 const submit = document.querySelector('.submit')
+const containers = document.querySelectorAll('.container')
+let test
 
 buttons.addEventListener('click', e => {
     const el = e.target
     if(el.classList.contains('score')){
         scores.forEach(value => value.classList.remove('pressed'))
         el.classList.add('pressed')
+        test = el.innerText
     }
 })
 
-submit.addEventListener('click', e => {
-    
+submit.addEventListener('click', () => {
+    if(test){
+        const score = document.querySelector('.selectedScore')
+        containers[0].classList.add('none')
+        containers[1].classList.remove('none')
+        score.innerText = `You selected ${test} out of 5`
+    }
 })
 })()
