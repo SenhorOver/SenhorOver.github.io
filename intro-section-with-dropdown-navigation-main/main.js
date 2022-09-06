950d9031330ab54a9fca0196b23966d0f111ac64
Marcos Vinicius Silva <MrcsVS@outlook.com>
2022-09-06 19:28:05 -0300

    upd project

--- intro-section-with-dropdown-navigation-main/main.js
+++ intro-section-with-dropdown-navigation-main/main.js
@@ -2,6 +2,10 @@
 const Main = {
     mobileMenu: document.querySelector('.bg'),
     transparentBg: document.querySelector('.transparent-bg'),
+    openMobileSubmenu: document.querySelector('#features'),
+    openMobileSubmenu1: document.querySelector('#company'),
+    mobileSubMenu: document.querySelectorAll('.submenu'),
+
 
     init(){
         this.selectors()
@@ -11,14 +15,15 @@ const Main = {
     selectors(){
         this.closeMenu = document.querySelector('#close-menu')
         this.openMenu = document.querySelector('#open-menu')
-        this.mobileFeatures = document.querySelector('#features')
+        this.mobileFeatures = document.querySelector('#features-click')
+        this.mobileCompany = document.querySelector('#company-click')
     },
 
     events(){
         this.closeMenu.onclick = this.Events.svgClick_OpenCloseMenu.bind(this)
         this.openMenu.onclick = this.Events.svgClick_OpenCloseMenu.bind(this)
         this.mobileFeatures.onclick = this.Events.mobileClick_CollapsedExpanded.bind(this)
-    
+        this.mobileCompany.onclick = this.Events.mobileClick_CollapsedExpanded.bind(this)
     },
 
     Events: {
@@ -48,7 +53,38 @@ const Main = {
             }, 300)
         },
 
-        mobileClick_CollapsedExpanded(){
+        mobileClick_CollapsedExpanded(e){
+            let num = 1
+            const el = e.target
+            try{
+                const pai = el.nextSibling.nextSibling.children
+                const subMenu = el.nextSibling.nextSibling
+                if(el.classList.contains('closed')){
+                    el.classList.remove('removed')
+                    el.classList.add('open')
+                    subMenu.classList.remove('none')
+                    console.log(subMenu);
+                    for(let i of pai){
+                        i.style.transformOrigin = 'top center;'
+                        i.style.animation = `slideDown 300ms ${num * 60}ms ease-in-out forwards`
+                        i.classList.remove('none')
+                        num++
+                    }
+                    return el.classList.remove('closed')
+                }
+                for(let i of pai){
+                    i.style.transformOrigin = 'top center;'
+                    i.style.animation = `slideUp 300ms ${num * 60}ms ease-in-out forwards`
+                    num--
+                }
+                el.classList.add('removed')
+                el.classList.remove('open')
+                setTimeout(function(){
+                    subMenu.classList.add('none')
+                }, 200)
+                return el.classList.add('closed')
+                
+            } catch(e){}
             
         },
     }
