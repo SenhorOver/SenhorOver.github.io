20a01ba85773ef874b591199beac355cd0f8452d
Marcos Vinicius Silva <mrcsvs@outlook.com>
2023-02-20 13:52:16 -0300

    add switch page animation + about page

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ class Main{
         this.homeWords = ['FrontEnd.', 'Backend.', 'FullStack.']
         this.arrIndex = 0
         this.textInterval = null
+
+
+        // NAVBAR PAGES
+        this.pageNames = ['Home', 'Sobre', 'Habilidades', 'Experiência', 'Portfólio', 'Contato']
+        this.pages = document.querySelectorAll('.content')
     }
 
     init() {
@@ -20,10 +25,15 @@ class Main{
 
 
         // NAVBAR PAGES
+        this.btnNavbar = document.querySelectorAll('.nav-button');
     }
 
     bindEvents() {
          this.events().home.writing()
+
+         this.btnNavbar.forEach(button => {
+            button.onclick = this.events().navbarPages.btn_Click
+         })
     }
 
     events() {
@@ -56,6 +66,43 @@ class Main{
                             }
                         },70)
                     }, 400)
+                }
+            },
+
+            navbarPages: {
+                btn_Click: (e) => {
+                    const el = e.target
+                    if(el.localName !== 'span') return
+                    if(el.classList.contains('active')) return
+
+                    this.btnNavbar.forEach(button => {
+                        const span = button.firstElementChild.firstElementChild
+                        if(span.classList.contains('btn-active')) return this.events().utils.rmAddClass(span, 'btn-active', true)
+                    })
+                    el.classList.add('btn-active')
+
+                    const pageText = e.target.parentElement.nextElementSibling.innerText
+                    const indexPage = this.pageNames.indexOf(pageText)
+                    this.pages.forEach((page, index) => {
+                        if(index < indexPage) {
+                            this.events().utils.rmAddClass(page, 'page-up', false)
+                        }
+
+                        if(index >= indexPage) {
+                            this.events().utils.rmAddClass(page, 'page-up', true)
+                        }
+                    })
+                }
+            },
+
+
+
+
+
+            utils: {
+                rmAddClass: (el, className, rm) => {
+                    if(rm) return el.classList.remove(className)
+                    el.classList.add(className)
                 }
             }
 
